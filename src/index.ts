e75b58b225b5c2cd2390ad8582b04f1439189dfe
o-dasher <88356162+o-dasher@users.noreply.github.com>
2021-09-07 13:20:45 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ declare global {
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL
   })
 });
